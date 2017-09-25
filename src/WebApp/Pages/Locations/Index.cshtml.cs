7708c2c1884f4d8f0c5fe0552da6464b5f2c2ab6
Nicholas Myers <neekgreen@users.noreply.github.com>
2017-09-25 00:50:31 -0500

    initial commit

---
+++ src/WebApp/Pages/Locations/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApp.Pages.Locations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Hangfire;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPostAsync()
+        {
+            BackgroundJob.Enqueue(() => Console.WriteLine("this is a location job"));
+
+            return Page();
+        }
+    }
+}
---
+++ src/WebApp/Startup.cs
@@ -0,0 +1,53 @@
+namespace WebApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Hangfire;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddHangfire(config => config.UseSqlServerStorage(Configuration.GetConnectionString("HangFireConnectionString")));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
