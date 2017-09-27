73d718f730aa5f5eca9b5fa77871e887afbef068
Caleb Tucker <calebt@gmail.com>
2017-09-27 15:44:25 -0500

    added member info to check in response

--- src/WebApp/Api/LocationCheckins/LocationCheckinController.cs
+++ src/WebApp/Api/LocationCheckins/LocationCheckinController.cs
@@ -18,40 +18,13 @@ namespace WebApp.Api.LocationCheckins
         {
             this.mediator = mediator;
         }
-        
-//        // GET: api/values
-//        [HttpGet]
-//        public IEnumerable<string> Get()
-//        {
-//            return new string[] { "value1", "value2" };
-//        }
-//
-//        // GET api/values/5
-//        [HttpGet("{id}")]
-//        public string Get(int id)
-//        {
-//            return "value";
-//        }
-//
-//        
-//
-//        // PUT api/values/5
-//        [HttpPut("{id}")]
-//        public void Put(int id, [FromBody]string value)
-//        {
-//        }
-//
-//        // DELETE api/values/5
-//        [HttpDelete("{id}")]
-//        public void Delete(int id)
-//        {
-//        }
-        
+                
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]CheckinCommand value)
+        public CheckinResponse Post([FromBody]CheckinCommand value)
         {
-            this.mediator.Send(value);
+            var response = this.mediator.Send<CheckinResponse>(value);
+            return response.Result;
         }
     }
 }
