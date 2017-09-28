7c9dd6beb3590d9abc7861d835fcd9f5c5c19507
Maria Abreu <mabreu@daxko.com>
2017-09-28 10:15:17 -0500

    new

--- src/WebApp/Pages/Index.cshtml.cs
+++ src/WebApp/Pages/Index.cshtml.cs
@@ -6,13 +6,26 @@
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
-
+    using WebApp.Models;
 
     public class IndexModel : PageModel
     {
+        private readonly AppDbContext context;
+
+        public IndexModel(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public HomePageModel HomePageModel { get; set; }
+
         public void OnGet()
         {
-
+            HomePageModel = new HomePageModel(){
+                total_members = context.Members.Count(),
+                total_locations = context.Locations.Count(),
+                total_location_checkins = context.LocationCheckin.Count()
+            };
         }
     }
 }
