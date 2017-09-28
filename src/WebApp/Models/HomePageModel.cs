b326b425f5f654626a2739d72effcd26f918244e
Maria Abreu <mabreu@daxko.com>
2017-09-28 10:15:32 -0500

    new

---
+++ src/WebApp/Models/HomePageModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp.Models
+{
+    public class HomePageModel
+    {
+        public int total_members {get; set;}
+        public int total_locations {get; set;}
+        public int total_location_checkins {get; set;}
+    }
+}
